6c597e94a33174f23155423b1c16d991a05af792
Kim Gaeun <kge2015@gmail.com>
2026-04-08 18:05:05 +0900

    feat: 프로젝트 초기 설정 및 HelloController 추가

---
+++ src/main/java/io/github/gaeunamy/minwon_chatbot/MinwonChatbotApplication.java
@@ -0,0 +1,13 @@
+package io.github.gaeunamy.minwon_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinwonChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinwonChatbotApplication.class, args);
+	}
+
+}
